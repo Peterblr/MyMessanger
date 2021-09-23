d97823e378b091e3b6f29ec62c1d06bd1649198d
Peter Sviryd <Peter.blr@yandex.ru>
2021-09-23 18:39:12 +0300

    add Serialize message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+
 
 namespace MyMessanger
 {
@@ -6,10 +8,18 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
+            Message msg = new Message("RusAl", "Hello", DateTime.UtcNow);
 
-            Console.WriteLine("Start project!");
-            Console.WriteLine(msg.ToString());
+            string output = JsonConvert.SerializeObject(msg);
+
+            Console.WriteLine(output);
+
+            Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+
+            Console.WriteLine(deserializeMsg);
+
+            //{ "UserName":"RusAl","MessageText":"Hello","TimeStamp":"2021-09-23T15:36:10.706071Z"}
+            //RusAl < 9 / 23 / 2021 3:36:10 PM >: Hello
         }
     }
 }
