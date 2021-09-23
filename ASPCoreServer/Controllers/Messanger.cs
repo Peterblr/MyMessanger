c141bd65d2ce2bfed6542fbac86640eddfd49535
Peter Sviryd <Peter.blr@yandex.ru>
2021-09-23 19:34:39 +0300

    add message functions

--- ASPCoreServer/Controllers/Messanger.cs
+++ ASPCoreServer/Controllers/Messanger.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyMessanger;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +13,34 @@ namespace ASPCoreServer.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class Messanger : ControllerBase
-    {      
+    {
+        static List<Message> ListOfMessages = new List<Message>();
         // GET api/<Messanger>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Can i go IT???" + id.ToString();
+            string outputString = "Not found!";
+
+            if ((id < ListOfMessages.Count) && (id >= 0))
+            {
+                outputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+            Console.WriteLine(String.Format("message # {0} : {1}", id, outputString));
+
+            return outputString;
         }
 
         // POST api/<Messanger>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessages.Add(msg);
+            Console.WriteLine(String.Format("Total messages: {0} Send messages: {1}", ListOfMessages.Count, msg));
+            return new OkResult();
         }
     }
 }
