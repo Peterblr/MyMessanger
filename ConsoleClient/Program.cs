33decd2e44b64e5cb9990d300a1cb537ce01460e
Peter Sviryd <Peter.blr@yandex.ru>
2021-09-23 18:04:48 +0300

    Start project!

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,7 +6,7 @@ namespace MyMessanger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Start project.");
+            Console.WriteLine("Start project!");
         }
     }
 }
